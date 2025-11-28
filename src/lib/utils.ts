658547266f6e5e6b5647f7dd88277ab2291f6966
shabgardetanha34 <shabgardetanha34@yahoo.com>
2025-11-28 10:12:30 +0330

    Add missing Input & Progress components - final fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+// src/lib/utils.ts
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
